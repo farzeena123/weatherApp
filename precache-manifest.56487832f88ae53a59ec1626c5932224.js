c63014509fa4a1e242d1ef2f6a0cd7ab70648f2a
farzeena123 <farzeena2000@gmail.com>
2023-03-14 11:54:27 +0300

    Updates

---
+++ precache-manifest.56487832f88ae53a59ec1626c5932224.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4341c2581194fbc5cd4",
+    "url": "/weatherApp/static/css/main.8e86b29c.chunk.css"
+  },
+  {
+    "revision": "e4341c2581194fbc5cd4",
+    "url": "/weatherApp/static/js/main.e4341c25.chunk.js"
+  },
+  {
+    "revision": "47395c75adcab802558d",
+    "url": "/weatherApp/static/css/1.2b0e93d4.chunk.css"
+  },
+  {
+    "revision": "47395c75adcab802558d",
+    "url": "/weatherApp/static/js/1.47395c75.chunk.js"
+  },
+  {
+    "revision": "0ff3e20d12400699242a",
+    "url": "/weatherApp/static/js/runtime~main.0ff3e20d.js"
+  },
+  {
+    "revision": "0a24669c513f6ef2c426923ac12ac086",
+    "url": "/weatherApp/index.html"
+  }
+];
